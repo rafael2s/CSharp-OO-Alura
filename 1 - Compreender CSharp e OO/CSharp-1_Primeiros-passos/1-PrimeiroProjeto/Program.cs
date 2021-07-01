05dadc0769e7764a8fbe2da62d00a69bd807ba24
Rafael Souza <rafael_popo@hotmail.com>
2021-07-01 09:49:28 -0300

    Aula 1 e 2

---
+++ 1 - Compreender CSharp e OO/CSharp-1_Primeiros-passos/1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("A execução acabou. Tecle enter para finalizar......");
+            Console.ReadLine();
+        }
+    }
+
+}
