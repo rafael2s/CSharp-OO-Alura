ae3510a4a4a67a08d4fb3d7e77dc713e491c3f23
Rafael Souza <rafael_popo@hotmail.com>
2021-07-01 13:56:13 -0300

    Aula 04

---
+++ 1 - Compreender CSharp e OO/CSharp-1_Primeiros-passos/5-CaracteresETextos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5");
+
+            char primeiraLetra = ' ';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)61;
+            Console.WriteLine(primeiraLetra);   // Resultado: "=" (CODIGO ASCII 65 = '=')
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);   // Resultado: ">" (CODIGO ASCII 66 = '>')
+
+            string titulo = "Alura Cursos de tecnolocia " + 2021;
+            string cursosProgramacao = 
+@"- .NET + 
+- Java 
+- JavaScript";
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+       
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
