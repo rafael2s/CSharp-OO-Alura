4c6099d7b23faf291543090ae6956c209324053f
Rafael Souza <rafael_popo@hotmail.com>
2021-07-07 17:51:38 -0300

    Aula 05

---
+++ 1 - Compreender CSharp e OO/CSharp-2_Introducao-a-OO/ByteBank/06-ByteBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Rafael";
+            cliente.CPF = "444.555.666-77";
+            cliente.Profissao = "Programador";
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+            Console.ReadLine();
+        }
+    }
+}
