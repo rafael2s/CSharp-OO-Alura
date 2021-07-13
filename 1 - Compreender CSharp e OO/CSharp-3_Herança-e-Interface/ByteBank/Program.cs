20efbe7abebc254e9420a847d9253c3356af0060
Rafael Souza <rafael_popo@hotmail.com>
2021-07-13 17:04:05 -0300

    Aula 01

--- 1 - Compreender CSharp e OO/CSharp-3_Herança-e-Interface/ByteBank/Program.cs
+++ 1 - Compreender CSharp e OO/CSharp-3_Herança-e-Interface/ByteBank/Program.cs
@@ -7,14 +7,29 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            Funcionario carlos = new Funcionario(1);
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
 
+            Funcionario carlos = new Funcionario();
             carlos.Nome = "Carlos";
             carlos.CPF = "111.222.333-20";
-            carlos.Salario = 3212;
+            carlos.Salario = 2500;
+
+            gerenciador.Registrar(carlos);
 
             Console.WriteLine(carlos.Nome);
             Console.WriteLine(carlos.GetBonificacao());
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "444.333.222-11";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
 
             Console.ReadLine();
         }
