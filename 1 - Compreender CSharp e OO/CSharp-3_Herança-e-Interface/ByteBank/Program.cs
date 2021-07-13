25f8cd51f8cba0a1ea59ad21374d2d3da2f8adf0
Rafael Souza <rafael_popo@hotmail.com>
2021-07-13 16:42:32 -0300

    Start Parte 3

---
+++ 1 - Compreender CSharp e OO/CSharp-3_Herança-e-Interface/ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using ByteBank.Funcionarios;
+using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario(1);
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "111.222.333-20";
+            carlos.Salario = 3212;
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
