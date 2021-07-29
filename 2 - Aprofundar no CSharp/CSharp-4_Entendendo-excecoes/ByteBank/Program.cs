5fc81a9b7f1020e44cd17a2c7e59674a9ed8ced6
Rafael Souza <rafael_popo@hotmail.com>
2021-07-29 14:38:22 -0300

    start curso 4 - entendendo excecoes

---
+++ 2 - Aprofundar no CSharp/CSharp-4_Entendendo-excecoes/ByteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodo();
+            Console.ReadLine();
+        }
+        //Teste com a cadeia de chamada:
+        //Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(2);
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            return numero / divisor;
+        }
+    }
+}
