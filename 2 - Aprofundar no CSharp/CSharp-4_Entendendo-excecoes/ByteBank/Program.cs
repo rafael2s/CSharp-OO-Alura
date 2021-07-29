13f33c6a615c8572ee6681ab3713fcc371fca6c3
Rafael Souza <rafael_popo@hotmail.com>
2021-07-29 15:57:33 -0300

    Aula 02

--- 2 - Aprofundar no CSharp/CSharp-4_Entendendo-excecoes/ByteBank/Program.cs
+++ 2 - Aprofundar no CSharp/CSharp-4_Entendendo-excecoes/ByteBank/Program.cs
@@ -14,9 +14,14 @@ namespace ByteBank
             {
                 Metodo();
             }
-            catch(NullReferenceException erro)
+            catch (DivideByZeroException e)
             {
-                Console.WriteLine(erro.StackTrace);
+                Console.WriteLine("Não é possível divisão por zero.");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
                 Console.WriteLine("Aconteceu um error!");
             }
             
@@ -32,27 +37,21 @@ namespace ByteBank
 
         private static void TestaDivisao(int divisor)
         {
-            try
-            {
-                int resultado = Dividir(10, divisor);
-                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
-            }
-            catch(DivideByZeroException erro)
-            {
-                Console.WriteLine(erro.Message);
-                Console.WriteLine(erro.StackTrace);
-                Console.WriteLine("Não é possivel fazer divisão por 0!");
-            }
-                  
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);                
         }
 
         private static int Dividir(int numero, int divisor)
         {
-            ContaCorrente conta = null;
-           //Console.WriteLine(conta.Saldo);
-
-            return numero / divisor;
+            try
+            {
+                return numero / divisor;
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Exceção com numero=" + numero + " e divisor=" + divisor);
+                throw;
+            }
         }
-
     }
 }
