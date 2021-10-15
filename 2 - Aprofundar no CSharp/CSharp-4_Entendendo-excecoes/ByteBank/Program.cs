529e2d89a762482b3c171977f5620996c161f320
Rafael Souza <rafael_popo@hotmail.com>
2021-10-15 10:52:58 -0300

    Aula 06 - final

--- 2 - Aprofundar no CSharp/CSharp-4_Entendendo-excecoes/ByteBank/Program.cs
+++ 2 - Aprofundar no CSharp/CSharp-4_Entendendo-excecoes/ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +13,62 @@ namespace ByteBank
         {
             try
             {
+                CarregarContas();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("CATCH NO METODO MAIN");
+            }
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
+            Console.ReadLine();               
+        }
+
+        private static void CarregarContas()
+        {
+            using(LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
+            {
+             
+                leitor.LerProximaLinha();
+            }
+
+            // -------------------------------------------------------------------
+
+            //LeitorDeArquivo leitor = null;
+            //try // DECLARAÇÃO DO 'try' Pode ser com CATCH ou FINALLY - Sempre vai ter um dos 2 ou os 2 juntos.
+            //{
+            //    leitor = new LeitorDeArquivo("contas1.txt");
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            ////}
+            ////catch (IOException)
+            ////{                
+            ////    Console.WriteLine("Exceção do tipo IOException capturada e tratada!");
+            ////}
+            //finally
+            //{
+            //    Console.WriteLine("Executando o finally!");
+            //    if(leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }                
+            //}
+
+        }
+        
+        private static void TestaInnerException()
+        {
+            try
+            {
                 ContaCorrente conta1 = new ContaCorrente(1234, 67890);
                 ContaCorrente conta2 = new ContaCorrente(4321, 09876);
 
                 //conta1.Transferir(10000, conta2);
                 conta1.Sacar(10000);
             }
-            catch(OperacaoFinanceiraException e)
+            catch (OperacaoFinanceiraException e)
             {
                 Console.WriteLine(e.Message);
                 Console.WriteLine(e.StackTrace);
@@ -27,12 +77,7 @@ namespace ByteBank
                 //Console.WriteLine(e.InnerException.Message);
                 //Console.WriteLine(e.InnerException.StackTrace);
             }
-
-            Console.WriteLine("Execução finalizada. Tecle enter para sair");
-            Console.ReadLine();               
         }
-        //Teste com a cadeia de chamada:
-        //Metodo -> TestaDivisao -> Dividir
         private static void Metodo()
         {
             TestaDivisao(0);
