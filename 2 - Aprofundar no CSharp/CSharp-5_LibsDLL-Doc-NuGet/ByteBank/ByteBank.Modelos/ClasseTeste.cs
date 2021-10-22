35915629e8e1df9d4957d4494b6efe13f705d777
Rafael Souza <rafael_popo@hotmail.com>
2021-10-22 17:59:02 -0300

    Aula 02

---
+++ 2 - Aprofundar no CSharp/CSharp-5_LibsDLL-Doc-NuGet/ByteBank/ByteBank.Modelos/ClasseTeste.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    class ClasseTeste
+    {
+        public ClasseTeste()
+        {
+            ModificadoresTeste teste = new ModificadoresTeste();
+            teste.MetodoPublico();
+            //teste.MetodoPrivado();
+            //teste.MetodoProtegido();
+            teste.MetodoInterno();
+        }
+    }
+
+    public class ModificadoresTeste
+    {
+        public void MetodoPublico()
+        {
+            // Visível fora da classe "ModificadoresTeste"
+        }
+        public void MetodoPrivado()
+        {
+            // Visível apenas na classe "ModificadoresTeste"
+        }
+        public void MetodoProtegido()
+        {
+            // Visível fora da classe "ModificadoresTeste" e derivados
+        }
+
+        internal void MetodoInterno()
+        {
+            // Visível apenas para o projeto ByteBank,odelos!
+        }
+    }
+}
