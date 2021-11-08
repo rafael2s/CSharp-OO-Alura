edb9f440fd5aa93e61408a364942f29330526935
Rafael Souza <rafael_popo@hotmail.com>
2021-11-08 15:58:16 -0300

    Aula 04 e 05

--- 2 - Aprofundar no CSharp/CSharp-6_Strings-Exp_Reg-ClassObject/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ 2 - Aprofundar no CSharp/CSharp-6_Strings-Exp_Reg-ClassObject/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
@@ -12,6 +13,69 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            // "Olá, mundo!" é uma string
+            // WriteLine é um método público estático
+            // Console é uma classe pública
+
+            Console.WriteLine("Olá, mundo!");
+            Console.WriteLine(123);
+            Console.WriteLine(10.5);
+            Console.WriteLine(true);
+
+            object conta = new ContaCorrente(456, 6465646);
+            object desenvolvedor = new Desenvolvedor("123321");
+            
+            string contaToString = conta.ToString();
+
+            Console.WriteLine("Resultado " + contaToString);
+            Console.WriteLine(conta);
+
+            Cliente rafael_1 = new Cliente();
+            rafael_1.Nome = "Carlos";
+            rafael_1.CPF = "123.456.789-01";
+            rafael_1.Profissao = "Programador";
+
+            Cliente rafael_2 = new Cliente();
+            rafael_2.Nome = "Carlos";
+            rafael_2.CPF = "123.456.789-01";
+            rafael_2.Profissao = "Programador";
+
+            ContaCorrente conta_1 = new ContaCorrente(123, 456789);
+
+            if (rafael_1.Equals(conta_1))
+            {
+                Console.WriteLine("São iguais!");
+            }
+            else
+            {
+                Console.WriteLine("Não são iguais!");
+            }
+
+
+            Console.ReadLine();
+        }
+
+        static void TestaString()
+        {
+            // Olá, meu nome é Rafael e você pode entrar em contato comigo
+            // atravé do número 9578-5615!
+
+            // Meu nome é Rafael, me ligue em 9451-5452
+
+            //string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+            //string padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            //string padrao = "[0-9]{4,5}[-][0-9]{4}";
+            //string padrao = "[0-9]{4,5}[-]{0,1}[0-9]{4}";
+            //string padrao = "[0-9]{4,5}-{0,1}[0-9]{4}";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+            string textoDeTeste = "Meu nome é Rafael, me ligue em 99451-5452";
+
+            Match resultado = Regex.Match(textoDeTeste, padrao);
+
+            Console.WriteLine(resultado.Value);
+            Console.ReadLine();
+
+
             string urlTeste = "https://www.bytebank.com/cambio";
             int indiceByteBank = urlTeste.IndexOf("https://www.bytebank.com");
 
@@ -19,12 +83,11 @@ namespace ByteBank.SistemaAgencia
             Console.WriteLine(urlTeste.EndsWith("cambio"));
 
             Console.WriteLine(urlTeste.Contains("bytebank"));
-           
+
             Console.ReadLine();
-             
+
             // pagina?argumentos
             // 012345678
-
 
             // moedaOrigem=real&moedaDestino=real
             //                            |
@@ -49,7 +112,7 @@ namespace ByteBank.SistemaAgencia
             string termoBusca = "ra";
 
             Console.WriteLine(mensagemOrigem.ToLower());
-            
+
             Console.WriteLine(termoBusca.ToUpper());
 
 
@@ -76,7 +139,7 @@ namespace ByteBank.SistemaAgencia
 
             int indice = palavra.IndexOf(nomeArgumento);
             Console.WriteLine(indice);
-            
+
             Console.WriteLine("Tamanho da string nomeArgumento: " + nomeArgumento.Length);
 
             Console.WriteLine(palavra);
@@ -95,10 +158,6 @@ namespace ByteBank.SistemaAgencia
             Console.ReadLine();
 
 
-
-
-
-
             ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
 
             string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
@@ -110,10 +169,8 @@ namespace ByteBank.SistemaAgencia
             Console.WriteLine(url);
             string argumentos = url.Substring(indiceInterrogacao + 1);
             Console.WriteLine(argumentos);
-
-
-            Console.ReadLine();
         }
+
     }
 
 
