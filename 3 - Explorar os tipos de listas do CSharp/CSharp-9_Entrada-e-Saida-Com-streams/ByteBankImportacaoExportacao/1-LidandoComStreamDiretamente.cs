c187d6c85a6db59d6eb60e6f9a9c07eb0ba2a236
Rafael Souza <rafael_popo@hotmail.com>
2022-04-13 15:25:10 -0300

    Aula 2

---
+++ 3 - Explorar os tipos de listas do CSharp/CSharp-9_Entrada-e-Saida-Com-streams/ByteBankImportacaoExportacao/1-LidandoComStreamDiretamente.cs
@@ -0,0 +1,49 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO; // Input e Output
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void LidandoComFileStreamDiretamente()
+        {
+
+            var enderecoDoArquivo = "contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            {
+                var buffer = new byte[1024]; // 1KB
+                var numeroDeBytesLidos = -1;
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    EscreverBuffer(buffer, numeroDeBytesLidos);
+                }
+            }
+        }
+
+        static void EscreverBuffer(byte[] buffer, int byteLidos)
+        {
+            var utf8 = Encoding.Default;
+
+            var texto = utf8.GetString(buffer, 0, byteLidos);
+
+            Console.Write(texto);
+            /*
+            foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }
+            */
+
+        }
+    }
+}
